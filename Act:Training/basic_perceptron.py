9f51af5af98cc7dedb1b2d44232476530b3e30eb
PAOK-2001 <2001.paok@gmail.com>
2023-06-04 19:03:51 -0400

    Finished stochastic gradeint descent

--- Act:Training/basic_perceptron.py
+++ Act:Training/basic_perceptron.py
@@ -6,22 +6,17 @@
 
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
 class Perceptron:
-    def __init__(self, epochs, activation_funtion):
+    def __init__(self, epochs):
         self.bias = 0
         self.weights = None
         self.epochs = epochs
-        self.function = activation_funtion
 
     def activation_funtion(self, input):
-        if(self.function == 'relu'):
-            return np.max(0.0, input)
-        if(self.function == 'heaviside'):
-            return np.heaviside(input,0)
-        if(self.function == 'sigmoid'):
-            return 1.0/(1.0 + np.exp(-input))
+        return np.heaviside(input,0)
         
     def predict(self,input):
         weighted_input = np.dot(input, self.weights) + self.bias
@@ -31,7 +26,7 @@ class Perceptron:
         lr = learning_rate
         number_indexes = train.shape[0]
         number_features = train.shape[1]
-        self.weights = 2*np.random(number_features)-1
+        self.weights = 2*np.random.rand(number_features)-1
         accuracy = []
         for epoch in range(self.epochs):
             indexes = np.random.permutation(number_indexes)
@@ -42,11 +37,15 @@ class Perceptron:
                 test_prediction = self.predict(x_test)
                 acc = accuracy_score(y_test,test_prediction)
                 accuracy.append(acc)
-            if x_test is not None and y_test is not None:
-                #Display accuracy
-                accuracy = np.array(accuracy)
-                plt.plot( np.array(len(self.epochs)), accuracy)
-                plt.show()
+        if x_test is not None and y_test is not None:
+            #Display accuracy
+            accuracy = np.array(accuracy)
+            plt.plot(np.arange(self.epochs), accuracy)
+            plt.title("Accuracy vs Epochs")
+            plt.xlabel("Epochs")
+            plt.ylabel("Acurracy")
+            plt.savefig("./out/single_neuro_stochastic.png")
+            plt.show()
         return self.weights, self.bias
     
     def batch_gradient_descent(self, train, validate):
@@ -67,15 +66,16 @@ class Perceptron:
                 prediction = self.activation_funtion(weighted_input)
         return self.weights, self.bias
             
-    def train(self,training_type ,train, validate,x_test,y_test,learning_rate):
+    def train(self,training_type ,train, validate,x_test = None,y_test = None ,learning_rate = 0.001):
         if(training_type == 'stochastic'): return self.stochastic_gradient_descent(train, validate, x_test, y_test, learning_rate)
         if(training_type == 'batch'): return self.batch_gradient_descent(train, validate)
         if(training_type == 'mini_batch'): return self.mini_gradient_descent(train, validate)
-        
-
-
 
 if __name__ == "__main__":
-    model = Perceptron(100,'sigmoid')
-    model.train('stochastic')
-    
+    path = './data/misterious_data_1.txt'
+    data = np.loadtxt(path)
+    x = data[:,1:-1]
+    y = data[:,0]
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
+    model = Perceptron(300)
+    model.train('stochastic',X_train,y_train,X_test,y_test,0.001)
