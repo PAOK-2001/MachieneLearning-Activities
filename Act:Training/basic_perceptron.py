4e2ab6ca5544fcd7ad1f1760412f0734e33e654e
PAOK-2001 <2001.paok@gmail.com>
2023-06-04 22:32:44 -0400

    Implemented for SVM for sgd

--- Act:Training/basic_perceptron.py
+++ Act:Training/basic_perceptron.py
@@ -14,6 +14,7 @@ class Perceptron:
         self.bias = 0
         self.weights = None
         self.epochs = epochs
+        self.use_svm = use_svm
 
     def activation_funtion(self, input):
         return np.heaviside(input,0)
@@ -23,6 +24,7 @@ class Perceptron:
         return self.activation_funtion(weighted_input)
         
     def stochastic_gradient_descent(self, train, validate, x_test, y_test, learning_rate):
+        print(f"Starting training with {self.epochs} epochs and stochastic_gradient_descent")
         alpha = learning_rate
         number_indexes = train.shape[0]
         number_features = train.shape[1]
@@ -32,7 +34,11 @@ class Perceptron:
             indexes = np.random.permutation(number_indexes)
             for index in indexes:
                 curr_pred = self.predict(train[index, :])
-                self.weights = self.weights + alpha*(validate[index]-curr_pred)*train[index, :]
+                if(self.use_svm):
+                    if validate[index]*curr_pred < 1:
+                        self.weights = self.weights + alpha*validate[index]*train[index, :]
+                elif(not self.use_svm):
+                    self.weights = self.weights + alpha*(validate[index]-curr_pred)*train[index, :]
             if x_test is not None and y_test is not None:
                 test_prediction = self.predict(x_test)
                 acc = accuracy_score(y_test,test_prediction)
@@ -41,10 +47,14 @@ class Perceptron:
             #Display accuracy
             accuracy = np.array(accuracy)
             plt.plot(np.arange(self.epochs), accuracy)
-            plt.title("Accuracy vs Epochs")
             plt.xlabel("Epochs")
             plt.ylabel("Acurracy")
-            plt.savefig("./out/single_neuron_stochastic.png")
+            if(self.use_svm):
+                plt.title("Accuracy vs Epochs (SVM)")
+                plt.savefig("./out/single_neuron_stochastic_svm.png")
+            else: 
+                plt.title("Accuracy vs Epochs")
+                plt.savefig("./out/single_neuron_stochastic.png")
             plt.show()
         return self.weights, self.bias
     
@@ -52,7 +62,7 @@ class Perceptron:
         alpha = learning_rate
         number_indexes = train.shape[0]
         number_features = train.shape[1]
-        self.weights = 2*np.random.rand(number_features)-1
+        self.weights = np.ones(shape=(number_features))
         accuracy = []
         for epoch in range(self.epochs):
             indexes = np.random.permutation(number_indexes)
@@ -111,5 +121,5 @@ if __name__ == "__main__":
     x = data[:,1:-1]
     y = data[:,0]
     X_train, X_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state=42)
-    model = Perceptron(300)
+    model = Perceptron(300,True)
     model.train('stochastic',X_train,y_train,X_test,y_test,0.001)
