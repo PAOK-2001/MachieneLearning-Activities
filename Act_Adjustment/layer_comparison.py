085a238994575e28d13a2a1419cc50a6838511a9
PAOK-2001 <2001.paok@gmail.com>
2023-06-08 22:44:05 -0400

    Added regularization term comparison

--- Act_Adjustment/layer_comparison.py
+++ Act_Adjustment/layer_comparison.py
@@ -19,7 +19,7 @@ def kfold_accuracy(x, y, mlp):
     accuracy /= kf.get_n_splits()
     return accuracy
 
-def train_MLP_classifier(neurons_per_layer,layers):
+def MLP_from_size(neurons_per_layer,layers):
     size = (neurons_per_layer,)*layers
     MLP = MLPClassifier(hidden_layer_sizes=size,max_iter = 300,activation = "relu", solver = 'adam')
     data, target = load_wine(return_X_y=True)
@@ -30,12 +30,22 @@ def train_MLP_classifier(neurons_per_layer,layers):
     print('Accuracy: {:.2f}'.format(accuracy))
     return accuracy
 
+def MLP_from_regulator(regulator):
+    size = (20,)*5
+    MLP = MLPClassifier(hidden_layer_sizes=size,max_iter = 300,activation = "relu", solver = 'adam',alpha=regulator)
+    data, target = load_wine(return_X_y=True)
+    X_train, X_test, y_train, y_test = train_test_split(data, target, test_size=0.2, random_state=42)
+    MLP.fit(X_train, y_train)
+    accuracy = kfold_accuracy(data, target, MLP)
+    return accuracy
+
 if __name__ == "__main__":
-    neurons = 5
-    test_sizes = [4,6,8,10,30,40,50,80,90,100,120]
+    neurons = 20
+    test_sizes = np.arange(2,200,2)
+    print(test_sizes)
     acc = []
     for size in test_sizes:
-        accuracy = train_MLP_classifier(neurons,size)
+        accuracy = MLP_from_size(neurons,size)
         acc.append(accuracy)
     plt.figure(1)
     plt.plot(test_sizes, acc)
@@ -43,6 +53,34 @@ if __name__ == "__main__":
     plt.ylabel("Acurracy")
     plt.title(f"Layers vs Accuracy ({neurons} neurons per layer)")
     plt.savefig(f"./out/{neurons}_neurons.png")
+    regulators_small = np.arange(1e-5,1,1e-3) 
+    regulators_big = np.arange(1,100,10)
+    acc_small = []
+    for alpha in regulators_small:
+        accuracy = MLP_from_regulator(alpha)
+        acc_small.append(accuracy)
+    acc_big = []
+    for alpha in regulators_big:
+        accuracy = MLP_from_regulator(alpha)
+        acc_big.append(accuracy)
+    plt.figure(2)
+    plt.plot(regulators_small, acc_small)
+    plt.xlabel('Alpha')
+    plt.ylabel("Acurracy")
+    plt.title(f"Alpha vs Accuracy ({5} neurons per layer)")
+    plt.savefig(f"./out/small_alpha_comparison.png")
+    plt.figure(3)
+    plt.plot(regulators_big, acc_big)
+    plt.xlabel('Alpha')
+    plt.ylabel("Acurracy")
+    plt.title(f"Alpha vs Accuracy ({5} neurons per layer)")
+    plt.savefig(f"./out/big_alpha_comparison.png")
+
+
+    
+
+
+
 
         
 
